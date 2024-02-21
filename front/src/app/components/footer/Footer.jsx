7ca31c6636c6fb69b625a07a918c019e5dec5e20
Mai Vang <vmaineng@gmail.com>
2024-02-21 15:56:46 -0800

    updated footer

--- front/src/app/components/footer/Footer.jsx
+++ front/src/app/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+    <div className={styles.logo}> Logo</div>
+    <div className={styles.text}>
+    Lama creative thoughts &copy;{new Date().getFullYear()}. All rights reserved
+    </div>
+      </div>
   )
 }
 
--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import styles from "./home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1> Happiness</h1>
-        <p> Lorem ipsum</p>
+        <h1 className={styles.title}> Happiness</h1>
+        <p className = {styles.desc}> Lorem ipsum</p>
         <div className={styles.buttons}>
           <button>Learn more</button>
         </div>
       </div>
       <div className={styles.imageContainer}>
-        <Image src="hero.png" alt="hero" fill className={styles.heroImg}/>
+        <Image src="/hero.png" alt="hero" fill className={styles.heroImg} />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
